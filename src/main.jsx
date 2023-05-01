5118494de4b0fa5525548df15fcb95788654c88a
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2023-05-01 19:57:53 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { ThemeProvider } from "styled-components"
+import GlobalStyle from "./styles/global"
+import theme from "./styles/theme"
+import { Details } from "./pages/Details"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Details />
+    </ThemeProvider>
+  </React.StrictMode>
+)
