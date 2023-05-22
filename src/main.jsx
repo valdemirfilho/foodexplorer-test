61f68ce72a136865914ee3606b626073a2f5ed2f
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2023-05-21 21:28:25 -0300

    Alterações

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/global";
 import theme from "./styles/theme";
-import { Details } from "./pages/UserPages/Details";
+import { SignUp } from "./pages/LoginPages/SignUp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Details />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>
 );
